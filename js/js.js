94c9b3807dc2ed2a766eab2dd024cea9af4f1ce7
thbts <1205552014@qq.com>
2019-08-12 16:06:46 +0800

    001

---
+++ js/js.js
@@ -0,0 +1,10 @@
+//css
+document.write('<link rel="stylesheet" type="text/css" href="../css/base.css"/>');
+document.write('<link rel="stylesheet" type="text/css" href="../css/layouts.css"/>');
+
+
+//js
+document.write('<script src="../js/jquery.js"></script>');
+document.write('<script src="../js/flexible.js"></script>');
+document.write('<script src="../js/ajax.js"></script>');
+document.write('<script src="http://api.map.baidu.com/api?v=2.0&ak=zD8XjNeqt1V0aIRGtGbYFExx6MpC1Yif"></script>');
